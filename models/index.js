8deff25a5ea83bb835a30dfc3b391aa381dcdba9
PJLvadas <plivadas@gmail.com>
2015-04-23 19:55:32 -0400

    config and index.js changes

--- models/index.js
+++ models/index.js
@@ -6,7 +6,14 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (process.env.HEROKU_POSTGRESQL_GREEN_URL) {
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_GREEN_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      logging:  true
+    })
+  } else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
 fs

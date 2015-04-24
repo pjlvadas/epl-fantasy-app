bae2134c119e6cf31c1036f4b95a5cf3f463f7c3
PJLvadas <plivadas@gmail.com>
2015-04-23 21:04:20 -0400

    will keep adding more

--- models/leagues.js
+++ models/leagues.js
@@ -1,14 +1,28 @@
 "use strict";
 module.exports = function(sequelize, DataTypes) {
   var leagues = sequelize.define("leagues", {
-    league_name: DataTypes.STRING(20),
-    admin_id: DataTypes.INTEGER,
-    number_players: DataTypes.INTEGER
+    league_name: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      validate: {
+        notEmpty: {msg: 'Please name your league.'}
+      }
+
+    admin_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isInt: {msg: 'Please enter an integer'},
+      }
+    number_players: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: {msg: 'Please enter an integer'},
+        max: {args: [10], msg: '10 Teams is the maximum for a league.'}
+      }
   }, {
 
     underscored: true,
-
-    // timestamps: false,
      
     classMethods: {
       associate: function(models) {
